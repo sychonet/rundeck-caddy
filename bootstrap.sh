55a5ed056fa1df95147d2013e05ac9be3679300b
Abhay Joshi <Abhay.Joshi@round.glass>
2017-10-09 22:26:32 +0530

    Added Caddyfile and modified bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ yum -y install java-1.8.0
 rpm -Uvh http://repo.rundeck.org/latest.rpm 
 yum -y install rundeck 
 service rundeckd start
+cd /vagrant && tar -xvf /vagrant/caddy*.tar
+cp /vagrant/caddy /usr/local/bin/
+nohup /vagrant/caddy -conf /vagrant/Caddyfile & > /vagrant/nohup.out
